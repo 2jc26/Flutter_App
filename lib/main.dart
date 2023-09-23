883000c6047e276489868dcbac9c1b172f4576b7
Javier Cerino <j.cerino@uniandes.edu.co>
2023-09-23 01:25:32 -0500

    Login view completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
-import 'roomie_detail.dart';
+import 'package:giusseppe_flut/screens/login.dart';
+import 'widgets/drawer.dart';
+import 'screens/roomie_detail.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
@@ -39,21 +41,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-          widget.title,
-          textAlign: TextAlign.justify,
-        ),
-      ),
-      drawer: const CustomDrawer(),
-      body: MyScrollView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return const Scaffold(
+      body: Login(),
     );
   }
 }
