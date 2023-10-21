a40613b72bb3497ebedb064dcaf120378bff267a
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-10-21 11:43:26 -0500

    HouseList view fix

--- lib/screens/house_list.dart
+++ lib/screens/house_list.dart
@@ -9,12 +9,15 @@ import 'package:giusseppe_flut/widgets/information_card.dart';
 import '../widgets/drawer.dart';
 
 class HouseListView {
-  void refreshHouseListView(List<HouseModelUpdate> housesList, List<HouseModelUpdate> housesLikingList, List<HouseModelUpdate> housesSearchingList) {}
+  void refreshHouseListView(
+      List<HouseModelUpdate> housesList,
+      List<HouseModelUpdate> housesLikingList,
+      List<HouseModelUpdate> housesSearchingList) {}
 }
 
 class HouseList extends StatefulWidget {
-
-  const HouseList({super.key, required this.userId, required this.houseFilters});
+  const HouseList(
+      {super.key, required this.userId, required this.houseFilters});
 
   final String userId;
   final HouseSearchingModelUpdate? houseFilters;
@@ -25,23 +28,27 @@ class HouseList extends StatefulWidget {
 
 class _HouseListState extends State<HouseList> implements HouseListView {
   late HouseListPresenter houseListPresenter;
-  
+
   String? _userId;
 
   final TextEditingController _searchController = TextEditingController();
-  
+
+  HouseSearchingModelUpdate? _houseFilters;
   List<HouseModelUpdate>? _housesList;
   List<HouseModelUpdate>? _filteredHousesList;
   List<HouseModelUpdate>? _housesLikingList;
   List<HouseModelUpdate>? _housesSearchingList;
 
   @override
-  void refreshHouseListView(List<HouseModelUpdate> housesList, List<HouseModelUpdate> housesLikingList, List<HouseModelUpdate> housesSearchingList) {
+  void refreshHouseListView(
+      List<HouseModelUpdate> housesList,
+      List<HouseModelUpdate> housesLikingList,
+      List<HouseModelUpdate> housesSearchingList) {
     setState(() {
       _housesLikingList = housesLikingList;
       _housesList = housesList;
       _housesSearchingList = housesSearchingList;
-      if(housesSearchingList.isEmpty) {
+      if (_houseFilters == null) {
         _filteredHousesList = _housesList;
       } else {
         _filteredHousesList = _housesSearchingList;
@@ -52,7 +59,7 @@ class _HouseListState extends State<HouseList> implements HouseListView {
   void _onSearchTextChanged() {
     setState(() {
       if (_searchController.text.isEmpty) {
-        if(_housesSearchingList!.isEmpty) {
+        if (_housesSearchingList!.isEmpty) {
           _filteredHousesList = _housesList;
         } else {
           _filteredHousesList = _housesSearchingList;
@@ -72,14 +79,16 @@ class _HouseListState extends State<HouseList> implements HouseListView {
   void initState() {
     super.initState();
     _userId = widget.userId;
-    houseListPresenter = HouseListPresenter(widget.userId,widget.houseFilters);
+    _houseFilters = widget.houseFilters;
+    houseListPresenter = HouseListPresenter(widget.userId, widget.houseFilters);
     houseListPresenter.backView = this;
     _searchController.addListener(_onSearchTextChanged);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_housesList!.isNotEmpty && _userId != null) {
+    if ((_housesList!.isNotEmpty && _houseFilters == null) ||
+        (_housesSearchingList!.isNotEmpty && _houseFilters != null)) {
       return Scaffold(
         appBar: AppBar(
           backgroundColor: const Color(0xFF2E5EAA),
@@ -97,53 +106,159 @@ class _HouseListState extends State<HouseList> implements HouseListView {
           centerTitle: true,
         ),
         drawer: const CustomDrawer(),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              if (_housesSearchingList!.isEmpty)
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  "Houses that match your tastes",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              if (_housesSearchingList!.isEmpty)
-              HouseSection(houseList: _housesLikingList),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Text(
-                  "Houses",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SearchField(searchController: _searchController),
-              IconButton(
-                icon: const Icon(Icons.filter_list),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => AppartmentFilter(userId: _userId!)),
-                  );
-                },
-              ),
-              HouseSection(houseList: _filteredHousesList),
-            ],
-          ),
+        body: Column(
+          children: [
+            if (_houseFilters == null && _housesLikingList!.isNotEmpty)
+              const SizedBox(height: 10),
+            if (_houseFilters == null && _housesLikingList!.isNotEmpty)
+              HouseSection(
+                  userId: _userId,
+                  title: 'Liking Houses',
+                  housesList: _housesLikingList,
+                  flex: 1,
+                  filter: false,
+                  searchController: _searchController),
+            const SizedBox(height: 10),
+            HouseSection(
+                userId: _userId,
+                title: 'All Houses',
+                housesList: _filteredHousesList,
+                flex: 2,
+                filter: true,
+                searchController: _searchController),
+            const SizedBox(height: 10),
+          ],
         ),
       );
-    } else {
+    } else if (_housesList!.isEmpty && _houseFilters == null) {
       return const Scaffold(
         body: Center(
           child: CircularProgressIndicator(),
         ),
       );
+    } else {
+      return const NoHousesSearch();
     }
+  }
+}
+
+class NoHousesSearch extends StatelessWidget {
+  const NoHousesSearch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2E5EAA),
+        title: const Text(
+          'Senehouse',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        centerTitle: true,
+      ),
+      drawer: const CustomDrawer(),
+      body: const Padding(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          "There are no houses matching your search",
+          style: TextStyle(
+            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
+
+class FilterButton extends StatelessWidget {
+  const FilterButton({
+    super.key,
+    required String? userId,
+    required TextEditingController searchController,
+  })  : _userId = userId,
+        _searchController = searchController;
+
+  final String? _userId;
+  final TextEditingController _searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SearchField(searchController: _searchController),
+        IconButton(
+          icon: const Icon(Icons.filter_list),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AppartmentFilter(userId: _userId!)),
+            );
+          },
+        ),
+      ],
+    );
   }
 }
 
 class HouseSection extends StatelessWidget {
   const HouseSection({
+    super.key,
+    required String title,
+    required String? userId,
+    required List<HouseModelUpdate>? housesList,
+    required int flex,
+    required bool filter,
+    required TextEditingController searchController,
+  })  : _userId = userId,
+        _title = title,
+        _housesList = housesList,
+        _flex = flex,
+        _filter = filter,
+        _searchController = searchController;
+
+  final String? _userId;
+  final String _title;
+  final List<HouseModelUpdate>? _housesList;
+  final int _flex;
+  final bool _filter;
+  final TextEditingController _searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: _flex,
+      fit: FlexFit.tight,
+      child: Column(
+        children: [
+          Text(
+            _title,
+            style: const TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          if (_filter)
+            FilterButton(userId: _userId!, searchController: _searchController),
+          Expanded(
+            child: HouseElements(houseList: _housesList),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HouseElements extends StatelessWidget {
+  const HouseElements({
     super.key,
     required List<HouseModelUpdate>? houseList,
   }) : _houseList = houseList;
@@ -152,26 +267,22 @@ class HouseSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 250,
-      child: ListView.builder(
-        itemCount: _houseList?.length,
-        itemBuilder: ((context, index) {
-          return GestureDetector(
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) =>
-                    HouseDetail(house: _houseList![index]),
-              ));
-            },
-            child: InformationCard(
-              path: 'assets/images/house1.jpg',
-              stars: _houseList![index].rating,
-              text: _houseList![index].name,
-            ),
-          );
-        }),
-      ),
+    return ListView.builder(
+      itemCount: _houseList?.length,
+      itemBuilder: ((context, index) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => HouseDetail(house: _houseList![index]),
+            ));
+          },
+          child: InformationCard(
+            path: 'assets/images/house1.jpg',
+            stars: _houseList![index].rating,
+            text: _houseList![index].name,
+          ),
+        );
+      }),
     );
   }
 }
