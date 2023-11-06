815da8c2269c5649a5aedccdf38fcce1a63a9f8f
Javier Cerino <j.cerino@uniandes.edu.co>
2023-11-06 16:38:04 -0500

    Changes in the Houses types and backend service

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:giusseppe_flut/app_navigator.dart';
 
 import 'package:giusseppe_flut/auth/auth_repository.dart';
-import 'package:giusseppe_flut/screens/user_list.dart';
 import 'package:giusseppe_flut/service/connectivity_manager_service.dart';
 
 // Firebase
 import 'package:firebase_core/firebase_core.dart';
+import 'package:giusseppe_flut/session_cubit.dart';
 import 'package:giusseppe_flut/storage/storage_adapters/Objectbox/ObjectBox.dart';
 import 'firebase_options.dart';
 
