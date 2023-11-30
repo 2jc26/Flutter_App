15b7ddb6d731fda18fd6f728d7e6a639ab61f857
Marco Zuliani <m.zuliani@uniandes.edu.co>
2023-11-30 12:39:30 -0500

    Add StoredHouse Upload
    If House Stored, creates it

--- lib/presenter/house_list_presenter.dart
+++ lib/presenter/house_list_presenter.dart
@@ -1,3 +1,9 @@
+import 'dart:ffi';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:giusseppe_flut/models/houseSearch/house_searching_model_update.dart';
 import 'package:giusseppe_flut/repository/search_repository.dart';
 
@@ -14,6 +20,8 @@ class HouseListPresenter {
   List<HouseModelUpdate> housesSearchingList = [];
   late HouseListView _backView= HouseListView();
   HouseListPresenter(String? userId, HouseSearchingModelUpdate? houseFilters) {
+    // compute(_loadStoredHouseInIsolate, null);
+    _loadStoredHouse();
     getAllHouses();
     getLikingHouses(userId);
     if(houseFilters != null) {
@@ -71,6 +79,45 @@ class HouseListPresenter {
       rethrow;
     }
   }
+
+  Future<bool> _loadStoredHouse() async {
+    try {
+      dynamic storedHouse = await houseRepository.getStoredHouseLocalFile();
+      if (storedHouse != null) {
+        // String path = "/images_houses/${storedHouse.name}/${storedHouse.city}/${storedHouse.neighborhood}/${storedHouse.address}/";
+        // List<String> imagesUrls = await houseRepository.uploadHouseImages(path, storedHouse.images); //TODO Uncomment when storage bug is fixed
+        // houseRepository.createHouse(HouseModelUpdate.fromJson({...storedHouse, "images": imagesUrls}));
+        houseRepository.createHouse(HouseModelUpdate.fromJson({...storedHouse}));
+        _backView.refreshHouseListView(housesList,housesLikingList,housesSearchingList);
+        // Create a snackbar to show success on the creation of the house
+        Get.rawSnackbar(
+          messageText: const Text(
+            'House created successfully!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+          isDismissible: true,
+          duration: const Duration(days: 15),
+          backgroundColor: Colors.green[400]!,
+          icon: const Icon( Icons.check, color: Colors.white, size: 35,),
+          margin: EdgeInsets.zero,
+          snackStyle: SnackStyle.GROUNDED
+        );
+
+        return true;
+      }
+      return false;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<bool> _loadStoredHouseInIsolate(dynamic _) async {
+    return await _loadStoredHouse();
+  }
+
 
   set backView(HouseListView value) {
     _backView = value;
